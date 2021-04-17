a9df15e6eb4407da355dd74a523f07aca3b32309
hiroshi yamato <yamato@gmail.com>
2021-04-17 18:37:09 +0900

    done "Handlling MIDI Event"

--- HandlingMidiEventsTutorial/Source/MainComponent.cpp
+++ HandlingMidiEventsTutorial/Source/MainComponent.cpp
@@ -2,11 +2,52 @@
 
 //==============================================================================
 MainComponent::MainComponent()
+    :   keyboardComponent(keyboardState, juce::MidiKeyboardComponent::horizontalKeyboard),
+        startTime(juce::Time::getMillisecondCounterHiRes() * 0.001)
 {
     // Make sure you set the size of the component after
     // you add any child components.
-    setSize (800, 600);
-
+    setSize (600, 400);
+    
+    addAndMakeVisible(midiInputList);
+    midiInputList.setTextWhenNoChoicesAvailable("No MIDI Inputs Enabled");
+    auto midiInputs = juce::MidiInput::getAvailableDevices();
+    
+    juce::StringArray midiInputNames;
+    
+    for (auto input : midiInputs) {
+        midiInputNames.add(input.name);
+    }
+    
+    midiInputList.addItemList(midiInputNames, 1);
+    midiInputList.onChange = [this] { setMidiInput(midiInputList.getSelectedItemIndex()); };
+    
+    for (auto input : midiInputs) {
+        if (deviceManager.isMidiInputDeviceEnabled(input.identifier)) {
+            setMidiInput(midiInputs.indexOf(input));
+            break;
+        }
+    }
+    
+    if (midiInputList.getSelectedId() == 0) {
+        setMidiInput(0);
+    }
+    
+    addAndMakeVisible(keyboardComponent);
+    keyboardState.addListener(this);
+    
+    addAndMakeVisible(midiMessageBox);
+    midiMessageBox.setMultiLine(true);
+    midiMessageBox.setReturnKeyStartsNewLine(true);
+    midiMessageBox.setReadOnly(true);
+    midiMessageBox.setScrollbarsShown(true);
+    midiMessageBox.setCaretVisible(true);
+    midiMessageBox.setPopupMenuEnabled(true);
+    midiMessageBox.setColour(juce::TextEditor::backgroundColourId, juce::Colour(0x32ffffff));
+    midiMessageBox.setColour(juce::TextEditor::outlineColourId, juce::Colour(0x1c000000));
+    midiMessageBox.setColour(juce::TextEditor::shadowColourId, juce::Colour(0x16000000));
+    
+    
     // Some platforms require permissions to open input channels so request that here
     if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
         && ! juce::RuntimePermissions::isGranted (juce::RuntimePermissions::recordAudio))
@@ -72,4 +113,81 @@ void MainComponent::resized()
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    auto area = getLocalBounds();
+    
+    midiInputList.setBounds(area.removeFromTop(36).removeFromRight(getWidth() - 150).reduced(8));
+    keyboardComponent.setBounds(area.removeFromTop(80).reduced(8));
+    midiMessageBox.setBounds(area.reduced(8));
+    
+}
+
+void MainComponent::setMidiInput(int index)
+{
+    auto list = juce::MidiInput::getAvailableDevices();
+    
+    deviceManager.removeMidiInputDeviceCallback(list[lastInputIndex].identifier, this);
+    
+    auto newInput = list[index];
+    
+    if (! deviceManager.isMidiInputDeviceEnabled(newInput.identifier)) {
+        deviceManager.setMidiInputDeviceEnabled(newInput.identifier, true);
+    }
+    
+    deviceManager.addMidiInputDeviceCallback(newInput.identifier, this);
+    midiInputList.setSelectedId(index + 1, juce::dontSendNotification);
+    
+    lastInputIndex = index;
+}
+
+void MainComponent::handleIncomingMidiMessage(juce::MidiInput *source, const juce::MidiMessage &message)
+{
+    const juce::ScopedValueSetter<bool> scopedInputFlag(isAddingFromMidiInput, true);
+    keyboardState.processNextMidiEvent(message);
+    postMessageToList(message, source->getName());
+    
+}
+
+void MainComponent::handleNoteOn(juce::MidiKeyboardState *, int midiChannel, int midiNoteNumber, float velocity)
+{
+    if (!isAddingFromMidiInput) {
+        auto m = juce::MidiMessage::noteOn(midiChannel, midiNoteNumber, velocity);
+        m.setTimeStamp(juce::Time::getMillisecondCounterHiRes() * 0.001);
+        postMessageToList(m, "On-Screen Keyboard");
+    }
+}
+
+void MainComponent::handleNoteOff(juce::MidiKeyboardState *, int midiChannel, int midiNoteNumber, float /*velocity*/)
+{
+    if (!isAddingFromMidiInput) {
+        auto m = juce::MidiMessage::noteOff(midiChannel, midiNoteNumber);
+        m.setTimeStamp(juce::Time::getMillisecondCounterHiRes() * 0.001);
+        postMessageToList(m, "On-Screen Keyboard");
+    }
+}
+
+void MainComponent::postMessageToList(const juce::MidiMessage &message, const juce::String &source)
+{
+    (new IncomingMessageCallback(this, message, source))->post();
+}
+
+void MainComponent::logMessage(const juce::String &m)
+{
+    midiMessageBox.moveCaretToEnd();
+    midiMessageBox.insertTextAtCaret(m + juce::newLine);
+}
+
+void MainComponent::addMessageToList(const juce::MidiMessage &message, const juce::String &source)
+{
+    auto time = message.getTimeStamp() - startTime;
+    
+    auto hours = ((int)(time / 3600.0)) % 24;
+    auto minutes = ((int)(time / 60.0)) % 60;
+    auto seconds = ((int)(time)) % 60;
+    auto mills = ((int)(time * 1000.0)) % 1000;
+    
+    auto timecode = juce::String::formatted("%02d:%20d:%02d.%03d", hours, minutes, seconds, mills);
+    auto description = getMidiMessageDescription(message);
+    
+    juce::String midiMessageString(timecode + " - " + description + " (" + source + ")");
+    logMessage(midiMessageString);
 }
--- HandlingMidiEventsTutorial/Source/MainComponent.h
+++ HandlingMidiEventsTutorial/Source/MainComponent.h
@@ -7,7 +7,9 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::AudioAppComponent
+class MainComponent  :  public juce::AudioAppComponent,
+                        private juce::MidiInputCallback,
+                        private juce::MidiKeyboardStateListener
 {
 public:
     //==============================================================================
@@ -25,8 +27,81 @@ public:
 
 private:
     //==============================================================================
+    // This is used to dispach an incoming message to the message thread
+    class IncomingMessageCallback : public juce::CallbackMessage
+    {
+    public:
+        IncomingMessageCallback(MainComponent *o, const juce::MidiMessage &m, const juce::String &s)
+            : owner(o), message(m), source(s)
+        {}
+        
+        void messageCallback() override
+        {
+            if (owner != nullptr) {
+                owner->addMessageToList(message, source);
+            }
+        }
+        
+        Component::SafePointer<MainComponent> owner;
+        juce::MidiMessage message;
+        juce::String source;
+    };
+    
+    //==============================================================================
     // Your private member variables go here...
-
+    static juce::String getMidiMessageDescription(const juce::MidiMessage &m)
+    {
+        if (m.isNoteOn())
+            return "Note on " + juce::MidiMessage::getMidiNoteName(m.getNoteNumber(), true, true, 3);
+        if (m.isNoteOff())
+            return "Note off " + juce::MidiMessage::getMidiNoteName(m.getNoteNumber(), true, true, 3);
+        if (m.isProgramChange())
+            return "Program change " + juce::String(m.getProgramChangeNumber());
+        if (m.isPitchWheel())
+            return "Pitch wheel " + juce::String(m.getPitchWheelValue());
+        if (m.isAftertouch())
+            return "After touch " + juce::MidiMessage::getMidiNoteName(m.getNoteNumber(), true, true, 3);
+        if (m.isChannelPressure())
+            return "Channle pressure " + juce::String(m.getChannelPressureValue());
+        if (m.isAllNotesOff())
+            return "All notes off";
+        if (m.isAllSoundOff())
+            return "All sound off";
+        if (m.isMetaEvent())
+            return "Meta event";
+        
+        if (m.isController()) {
+            juce::String name(juce::MidiMessage::getControllerName(m.getControllerNumber()));
+            
+            if (name.isEmpty())
+                name = "[" + juce::String(m.getControllerNumber()) + "]";
+            
+            return "Controller" + name + ": " + juce::String(m.getControllerValue());
+        }
+        
+        return juce::String::toHexString(m.getRawData(), m.getRawDataSize());
+    }
+    
+    void setMidiInput(int index);
+    void handleIncomingMidiMessage(juce::MidiInput *source, const juce::MidiMessage &message) override;
+    void handleNoteOn(juce::MidiKeyboardState*, int midiChannel, int midiNoteNumber, float velocity) override;
+    void handleNoteOff(juce::MidiKeyboardState*, int midiChannel, int midiNoteNumber, float /*velocity*/) override;
+    void postMessageToList(const juce::MidiMessage &message, const juce::String &source);
+    void addMessageToList(const juce::MidiMessage &message, const juce::String &source);
+    void logMessage(const juce::String &m);
+    
+    
+    juce::AudioDeviceManager deviceManager;
+    juce::ComboBox midiInputList;
+    juce::Label midiInputListLabel;
+    int lastInputIndex = 0;
+    bool isAddingFromMidiInput = false;
+    
+    juce::MidiKeyboardState keyboardState;
+    juce::MidiKeyboardComponent keyboardComponent;
+    
+    juce::TextEditor midiMessageBox;
+    double startTime;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
